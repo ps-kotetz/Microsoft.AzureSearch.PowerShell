611db4505ae5305cde013be1beed117583c74395
Tetsuaki Kokura <tekokura@microsoft.com>
2017-04-08 23:51:07 +0900

    implemented almost required cmdlets

--- Microsoft.AzureSearch.PowerShell.psm1
+++ Microsoft.AzureSearch.PowerShell.psm1
@@ -15,58 +15,115 @@ $TypeValueTable = @{
     "Edm.GeographyPoint" = "[string]"
 }
 
+$ModuleTemplate = @'
+function Add-AzureSearch{8}Document{{
+    Param({1},{2})
+    $objectData=[ordered]@{{  
+        '@search.action'='upload';
+        {3}=${4};
+    }}
+    $nonKeyFields = $MyInvocation.BoundParameters.keys | Where-Object {{ $_ -ne "{4}" }}
+    foreach($field in $nonKeyFields)
+    {{
+        $objectData[$field] = $MyInvocation.BoundParameters[$field]
+    }}
+    $uploadObject = New-Object psobject |  Add-Member -NotePropertyName value -NotePropertyValue @(New-Object psobject -Property $objectData) -PassThru
+    $data = $uploadObject   | ConvertTo-Json
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+
+    $requestUri = "{5}indexes/{0}/docs/index{6}"
+    $requestHeaders = @{{
+        "api-key"="{7}"
+        "Content-Type" = "application/json"
+    }}
+    
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $requestHeaders -Body $body
+}}
+
+function Merge-AzureSearch{8}Document{{
+    Param({1},{2})
+    
+    $objectData=[ordered]@{{  
+        '@search.action'='merge';
+        {3}=${4};
+    
+    }}
+    $nonKeyFields = $MyInvocation.BoundParameters.keys | Where-Object {{ $_ -ne "{4}"}}
+    foreach($field in $nonKeyFields)
+    {{
+        $objectData[$field] = $MyInvocation.BoundParameters[$field]
+    }}
+    $uploadObject = New-Object psobject |  Add-Member -NotePropertyName value -NotePropertyValue @(New-Object psobject -Property $objectData) -PassThru
+    $data = $uploadObject   | ConvertTo-Json
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+    $requestUri = "{5}indexes/{0}/docs/index{6}"
+    $requestHeaders = @{{
+        "api-key"="{7}"
+        "Content-Type" = "application/json"
+    }}
+    
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $requestHeaders -Body $body
+}}
+
+function Remove-AzureSearch{8}Document{{
+    Param({1})
+    
+    $objectData=[ordered]@{{  
+        '@search.action'='delete';
+        {3}=${4};
+    
+    }}
+    $uploadObject = New-Object psobject |  Add-Member -NotePropertyName value -NotePropertyValue @(New-Object psobject -Property $objectData) -PassThru
+    $data = $uploadObject   | ConvertTo-Json
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+
+    $requestUri = "{5}indexes/{0}/docs/index{6}"
+    $requestHeaders = @{{
+        "api-key"="{7}"
+        "Content-Type" = "application/json"
+    }}
+    
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $requestHeaders -Body $body
+}}
+'@
+
+
 
 function Update-AzureSearchSubModule{
     $uri = $Script:AzureSearchService + "indexes" + $Script:AzureSearchAPIVersion
     $global:indexList = (Invoke-WebRequest -Headers $BaseRequestHeaders -Uri $uri -Method Get).Content | ConvertFrom-Json
 
     foreach($indexInfo in $global:indexList.Value){
-        # Key field top
-        $fieldData = $indexInfo.fields | Sort-Object -Descending key
-        $indexName = $indexInfo.name
 
+        $indexName =  $indexInfo.name
+        $UpperindexName = [char]::ToUpper($indexInfo.name[0]) + $indexInfo.name.Substring(1)
         $keyField = $indexInfo.fields | Where-Object key -eq "true"
         $nonKeyField = $indexInfo.fields | Where-Object key -ne "true"
 
-        $paramDef = ( $TypeValueTable[$keyField.type] + '$KeyFeild_' + $keyField.name) + "," +   (($nonKeyField | % {$TypeValueTable[$_.type]  + '$' + $_.name}) -join ",")
-        $hashDef =  $keyField.name + '=$KeyFeild_' + $keyField.name + ";" +  (($nonKeyField | % {$_.name + '=$' + $_.name}) -join ";")
+        $keyFieldParameterName = 'KeyFeild_' + $keyField.name
+        $paramDefForKeyField = ( $TypeValueTable[$keyField.type] + '$' + $keyFieldParameterName) 
+        
+        $paramDefForNonKeyField = ($nonKeyField | % {$TypeValueTable[$_.type]  + '$' + $_.name}) -join "," 
+        $hashDef =  $keyField.name + '=$' + $keyFieldParameterName  + ";" +  (($nonKeyField | % {$_.name + '=$' + $_.name}) -join ";")
 
-        $moduleDefinition = @'
+        $moduleDefinition =  $ModuleTemplate -f @(
+                                                    $indexName,
+                                                    $paramDefForKeyField,
+                                                    $paramDefForNonKeyField,
+                                                    $keyField.name,
+                                                    $keyFieldParameterName,
+                                                    $AzureSearchService,
+                                                    $AzureSearchAPIVersion,
+                                                    $AzureSearchKey,
+                                                    $UpperindexName
+                                                )
 
-function Set-AzureSearch{0}Document{{
-    Param([ValidateSet("upload", "merge","mergeOrUpload","delete")][string]$Action="upload",{1})
-    
-    $objectData=[ordered]@{{  
-        '@search.action'=$Action;
-        {2}=${3};
-    
-    }}
-    $nonKeyFields = $MyInvocation.BoundParameters.keys | Where-Object {{ ($_ -ne "Action") -and ($_ -ne "{3}") }}
-    foreach($field in $nonKeyFields)
-    {{
-        $objectData[$field] = $MyInvocation.BoundParameters[$field]
-    }}
-    $uploadObject = New-Object psobject |  Add-Member -NotePropertyName value -NotePropertyValue @(New-Object psobject -Property $objectData) -PassThru
-    $uploadObject 
-    $data = $uploadObject   | ConvertTo-Json
-    $data
-    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
-
-    $requestUri = "{4}indexes/{0}/docs/index{5}"
-    $requestHeaders = @{{
-        "api-key"="{6}"
-        "Content-Type" = "application/json"
-    }}
-    
-    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $requestHeaders -Body $body
-}}
-'@ -f @($indexName,$paramDef,$keyField.name,('KeyFeild_' + $keyField.name),$AzureSearchService,$AzureSearchAPIVersion,$AzureSearchKey)
-
-    Write-Verbose $moduleDefinition
-    Write-Verbose "Update-AzureSearchSubModule"
+        Write-Verbose $moduleDefinition
+        Write-Verbose "Update-AzureSearchSubModule"
+        if((Get-Module ("Microsoft.AzureSearch.PowerShell." + $indexName)) -ne $null){
+            Remove-Module ("Microsoft.AzureSearch.PowerShell." + $indexName)
+        }
         New-Module -Name ("Microsoft.AzureSearch.PowerShell." + $indexName) -ScriptBlock ([scriptblock]::Create($moduleDefinition)) | Import-Module -Global
-
-        Get-Module
     }
 }
 function Connect-AzureSearch{
@@ -84,10 +141,7 @@ function Connect-AzureSearch{
         "Content-Type" = "application/json"
     }
     Write-Verbose -Message ("Connect-AzureSearch")
-    #New-Module -ScriptBlock -Name 
-    #https://[service name].search.windows.net/indexes?api-version=[api-version]
     Update-AzureSearchSubModule -Verbose
-    
 }
 
 
@@ -127,9 +181,6 @@ function New-AzureSearchField{
 
 
 
-
-#$body = [System.Text.Encoding]::UTF8.GetBytes($data)
-
 function New-AzureSearchIndex{
     Param(
         [Parameter(Mandatory=$true)]
@@ -142,18 +193,31 @@ function New-AzureSearchIndex{
         name = $Name
         fields= $Fields
     }
-
-
     $indexData = New-Object psobject -Property $props
     $data = $indexData | ConvertTo-Json
     $body = [System.Text.Encoding]::UTF8.GetBytes($data)
     Invoke-WebRequest -Uri $requestUri -Method Post -Headers $BaseRequestHeaders -Body $body
+    Update-AzureSearchSubModule
 }
 
-function Invoke-AzureSearch
+function Search-AzureSearch
 {
-    Param($Query)
+    Param([string]$IndexName,[string]$SearchString,[string]$FieldSelection="*",[string]$Filter)
+    $requestUri = $AzureSearchService + "indexes/" + $IndexName + "/docs/search" + $AzureSearchAPIVersion
+    $searchObj=[ordered]@{
+        search = $SearchString
+        select = $FieldSelection 
+    }
+    if($MyInvocation.BoundParameters.ContainsKey("filter"))
+    {
+        $searchObj.filter = $Filter
+    }
 
+    $indexData = New-Object psobject -Property $searchObj
+    $data = $indexData | ConvertTo-Json
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+    $result = Invoke-WebRequest -Uri $requestUri -Method Post -Headers $BaseRequestHeaders -Body $body
+    $result.content | ConvertFrom-Json | ForEach-Object value
 }
 
 function Remove-AzureSearchIndex {
@@ -180,106 +244,3 @@ function Get-AzureSearchIndex {
     $result = Invoke-WebRequest -Uri $uri -Headers $BaseRequestHeaders -Method Get
     $result.content | ConvertFrom-Json
 }
-
-<#
-New-AzureSearchField
-New-AzureSearchIndex
-Update-AzureSearchIndex
-Delete-AzureSearchIndex
-Get-AzureSearchStatistics
-Analyze-AzureSearchText
-Get-AzureSearchAnalyzedText
-Invoke-WebRequest -Method Post
-
-Add-AzureSearchDocument
-Update-AzureSearchDocument
-Delete-AzureSearchDocument
-Get-AzureSearchDocument
-
-Search-AzureSearch
-#>
-
-function Add-AzureSearchDocument{
-    Param([string]$Name)
-    $requestUri = $AzureSearchService + "indexes" + $AzureSearchAPIVersion
-    $props =[ordered]@{
-        name = $Name
-        fields= $Fields
-    }
-
-
-    $indexData = New-Object psobject -Property $props
-    $data = $indexData | ConvertTo-Json
-    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
-    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $BaseRequestHeaders -Body $body
-}
-
-$sampleData=@'
-{
-    "value": [
-        {
-            "@search.action": "upload",
-            "hotelId": "1",
-            "baseRate": 199.0,
-            "description": "Best hotel in town",
-            "description_fr": "Meilleur hôtel en ville",
-            "hotelName": "Fancy Stay",
-            "category": "Luxury",
-            "tags": ["pool", "view", "wifi", "concierge"],
-            "parkingIncluded": false,
-            "smokingAllowed": false,
-            "lastRenovationDate": "2010-06-27T00:00:00Z",
-            "rating": 5,
-            "location": { "type": "Point", "coordinates": [-122.131577, 47.678581] }
-        },
-        {
-            "@search.action": "upload",
-            "hotelId": "2",
-            "baseRate": 79.99,
-            "description": "Cheapest hotel in town",
-            "description_fr": "Hôtel le moins cher en ville",
-            "hotelName": "Roach Motel",
-            "category": "Budget",
-            "tags": ["motel", "budget"],
-            "parkingIncluded": true,
-            "smokingAllowed": true,
-            "lastRenovationDate": "1982-04-28T00:00:00Z",
-            "rating": 1,
-            "location": { "type": "Point", "coordinates": [-122.131577, 49.678581] }
-        },
-        {
-            "@search.action": "mergeOrUpload",
-            "hotelId": "3",
-            "baseRate": 129.99,
-            "description": "Close to town hall and the river"
-        },
-        {
-            "@search.action": "delete",
-            "hotelId": "6"
-        }
-    ]
-}
-'@
-
-
-
-
-$sampleData=@'
-{
-    "name": "hotels",  
-    "fields": [
-        {"name": "hotelId", "type": "Edm.String", "key": true, "searchable": false, "sortable": false, "facetable": false},
-        {"name": "baseRate", "type": "Edm.Double"},
-        {"name": "description", "type": "Edm.String", "filterable": false, "sortable": false, "facetable": false},
-        {"name": "description_fr", "type": "Edm.String", "filterable": false, "sortable": false, "facetable": false, "analyzer": "fr.lucene"},
-        {"name": "hotelName", "type": "Edm.String", "facetable": false},
-        {"name": "category", "type": "Edm.String"},
-        {"name": "tags", "type": "Collection(Edm.String)"},
-        {"name": "parkingIncluded", "type": "Edm.Boolean", "sortable": false},
-        {"name": "smokingAllowed", "type": "Edm.Boolean", "sortable": false},
-        {"name": "lastRenovationDate", "type": "Edm.DateTimeOffset"},
-        {"name": "rating", "type": "Edm.Int32"},
-        {"name": "location", "type": "Edm.GeographyPoint"}
-    ]
-}
-'@
