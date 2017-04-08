c6c82928e1c440c0f6bdf0376acf9f5a130b6237
Tetsuaki Kokura <tekokura@microsoft.com>
2017-04-08 20:54:22 +0900

    implemented set-azuredocument cmdlet

--- Microsoft.AzureSearch.PowerShell.psm1
+++ Microsoft.AzureSearch.PowerShell.psm1
@@ -1,25 +1,95 @@
-﻿# $AzureSearchKey = 
+﻿
 $AzureSearchKey = ""
-#$AzureSearchURL = "https://psazuresearch.search.windows.net/indexes?api-version=2016-09-01"
 $AzureSearchService = ""
 $AzureSearchAPIVersion=""
-#https://psazuresearch.search.windows.net/indexes?api-version=2016-09-01
+$BaseRequestHeaders=$null
 
+$TypeValueTable = @{
+    "Edm.String" = "[String]"
+    "Collection(Edm.String)"="[String[]]" 
+    "Edm.Boolean"="[Boolean]"
+    "Edm.Int32" = "[Int32]"
+    "Edm.Int64" = "[Int64]"
+    "Edm.Double"= "[Double]"
+    "Edm.DateTimeOffset"="[DateTimeOffset]"
+    "Edm.GeographyPoint" = "[string]"
+}
+
+
+function Update-AzureSearchSubModule{
+    $uri = $Script:AzureSearchService + "indexes" + $Script:AzureSearchAPIVersion
+    $global:indexList = (Invoke-WebRequest -Headers $BaseRequestHeaders -Uri $uri -Method Get).Content | ConvertFrom-Json
+
+    foreach($indexInfo in $global:indexList.Value){
+        # Key field top
+        $fieldData = $indexInfo.fields | Sort-Object -Descending key
+        $indexName = $indexInfo.name
+
+        $keyField = $indexInfo.fields | Where-Object key -eq "true"
+        $nonKeyField = $indexInfo.fields | Where-Object key -ne "true"
+
+        $paramDef = ( $TypeValueTable[$keyField.type] + '$KeyFeild_' + $keyField.name) + "," +   (($nonKeyField | % {$TypeValueTable[$_.type]  + '$' + $_.name}) -join ",")
+        $hashDef =  $keyField.name + '=$KeyFeild_' + $keyField.name + ";" +  (($nonKeyField | % {$_.name + '=$' + $_.name}) -join ";")
+
+        $moduleDefinition = @'
+
+function Set-AzureSearch{0}Document{{
+    Param([ValidateSet("upload", "merge","mergeOrUpload","delete")][string]$Action="upload",{1})
+    
+    $objectData=[ordered]@{{  
+        '@search.action'=$Action;
+        {2}=${3};
+    
+    }}
+    $nonKeyFields = $MyInvocation.BoundParameters.keys | Where-Object {{ ($_ -ne "Action") -and ($_ -ne "{3}") }}
+    foreach($field in $nonKeyFields)
+    {{
+        $objectData[$field] = $MyInvocation.BoundParameters[$field]
+    }}
+    $uploadObject = New-Object psobject |  Add-Member -NotePropertyName value -NotePropertyValue @(New-Object psobject -Property $objectData) -PassThru
+    $uploadObject 
+    $data = $uploadObject   | ConvertTo-Json
+    $data
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+
+    $requestUri = "{4}indexes/{0}/docs/index{5}"
+    $requestHeaders = @{{
+        "api-key"="{6}"
+        "Content-Type" = "application/json"
+    }}
+    
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $requestHeaders -Body $body
+}}
+'@ -f @($indexName,$paramDef,$keyField.name,('KeyFeild_' + $keyField.name),$AzureSearchService,$AzureSearchAPIVersion,$AzureSearchKey)
+
+    Write-Verbose $moduleDefinition
+    Write-Verbose "Update-AzureSearchSubModule"
+        New-Module -Name ("Microsoft.AzureSearch.PowerShell." + $indexName) -ScriptBlock ([scriptblock]::Create($moduleDefinition)) | Import-Module -Global
+
+        Get-Module
+    }
+}
 function Connect-AzureSearch{
     Param(
         [Parameter(Mandatory=$true)][string]$Key,
         [Parameter(Mandatory=$true)][string]$ServiceName,
         [string]$APIVersion="api-version=2016-09-01"
-        )
-        $AzureSearchKey = $Key
-        $Script:AzureSearchService = "https://" + $ServiceName + ".search.windows.net/"
-        $Script:AzureSearchAPIVersion = "?" + $APIVersion
-        $Script:AzureSearchKey = $Key
-}
-
-function Add-AzureSearchDocument{
+    )
+    $AzureSearchKey = $Key
+    $Script:AzureSearchService = "https://" + $ServiceName + ".search.windows.net/"
+    $Script:AzureSearchAPIVersion = "?" + $APIVersion
+    $Script:AzureSearchKey = $Key
+    $Script:BaseRequestHeaders = @{
+        "api-key"=$Script:AzureSearchKey
+        "Content-Type" = "application/json"
+    }
+    Write-Verbose -Message ("Connect-AzureSearch")
+    #New-Module -ScriptBlock -Name 
+    #https://[service name].search.windows.net/indexes?api-version=[api-version]
+    Update-AzureSearchSubModule -Verbose
     
 }
+
 
 function New-AzureSearchField{
     Param(
@@ -72,15 +142,12 @@ function New-AzureSearchIndex{
         name = $Name
         fields= $Fields
     }
-    $headers = @{
-        "api-key"=$AzureSearchKey
-        "Content-Type" = "application/json"
-    }
+
 
     $indexData = New-Object psobject -Property $props
     $data = $indexData | ConvertTo-Json
     $body = [System.Text.Encoding]::UTF8.GetBytes($data)
-    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $Headers -Body $body
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $BaseRequestHeaders -Body $body
 }
 
 function Invoke-AzureSearch
@@ -90,13 +157,28 @@ function Invoke-AzureSearch
 }
 
 function Remove-AzureSearchIndex {
-    Param([string]$IndexName)
-    $AppStr = "indexes/" + $IndexName
+    [CmdletBinding(
+                  SupportsShouldProcess=$true, 
+                  PositionalBinding=$false,
+                  HelpUri = 'http://www.microsoft.com/',
+                  ConfirmImpact='Medium')]
+    Param([string]$Name)
+    $AppStr = "indexes/" + $Name
 
     $URI = $AzureSearchService + $AppStr + $AzureSearchAPIVersion
-    $URI
+    Write-Verbose -Message ("Request URL : " + $URI)
   #   "https://psazuresearch.search.windows.net/indexes/hotels?api-version=2016-09-01"
-    Invoke-WebRequest -Uri $URI -Headers $Headers -Method Delete
+    Invoke-WebRequest -Uri $URI -Headers $BaseRequestHeaders -Method Delete
+}
+
+function Get-AzureSearchIndex {
+    Param([string]$Name)
+    $appStr = "indexes/" + $Name
+    $uri = $AzureSearchService + $appStr + $AzureSearchAPIVersion
+    Write-Verbose -Message ("Request URL : " + $uri)
+  #   "https://psazuresearch.search.windows.net/indexes/hotels?api-version=2016-09-01"
+    $result = Invoke-WebRequest -Uri $uri -Headers $BaseRequestHeaders -Method Get
+    $result.content | ConvertFrom-Json
 }
 
 <#
@@ -116,6 +198,68 @@ Get-AzureSearchDocument
 
 Search-AzureSearch
 #>
+
+function Add-AzureSearchDocument{
+    Param([string]$Name)
+    $requestUri = $AzureSearchService + "indexes" + $AzureSearchAPIVersion
+    $props =[ordered]@{
+        name = $Name
+        fields= $Fields
+    }
+
+
+    $indexData = New-Object psobject -Property $props
+    $data = $indexData | ConvertTo-Json
+    $body = [System.Text.Encoding]::UTF8.GetBytes($data)
+    Invoke-WebRequest -Uri $requestUri -Method Post -Headers $BaseRequestHeaders -Body $body
+}
+
+$sampleData=@'
+{
+    "value": [
+        {
+            "@search.action": "upload",
+            "hotelId": "1",
+            "baseRate": 199.0,
+            "description": "Best hotel in town",
+            "description_fr": "Meilleur hôtel en ville",
+            "hotelName": "Fancy Stay",
+            "category": "Luxury",
+            "tags": ["pool", "view", "wifi", "concierge"],
+            "parkingIncluded": false,
+            "smokingAllowed": false,
+            "lastRenovationDate": "2010-06-27T00:00:00Z",
+            "rating": 5,
+            "location": { "type": "Point", "coordinates": [-122.131577, 47.678581] }
+        },
+        {
+            "@search.action": "upload",
+            "hotelId": "2",
+            "baseRate": 79.99,
+            "description": "Cheapest hotel in town",
+            "description_fr": "Hôtel le moins cher en ville",
+            "hotelName": "Roach Motel",
+            "category": "Budget",
+            "tags": ["motel", "budget"],
+            "parkingIncluded": true,
+            "smokingAllowed": true,
+            "lastRenovationDate": "1982-04-28T00:00:00Z",
+            "rating": 1,
+            "location": { "type": "Point", "coordinates": [-122.131577, 49.678581] }
+        },
+        {
+            "@search.action": "mergeOrUpload",
+            "hotelId": "3",
+            "baseRate": 129.99,
+            "description": "Close to town hall and the river"
+        },
+        {
+            "@search.action": "delete",
+            "hotelId": "6"
+        }
+    ]
+}
+'@
 
 
 
